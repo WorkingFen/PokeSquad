7985f0a51c5428d0c8b001ad10a85ec52338bf7d
Mateusz <nologin90@gmail.com>
2020-01-13 22:21:30 +0100

    final roulette tests

--- algo/main.py
+++ algo/main.py
@@ -37,10 +37,9 @@ def mutate(offspring: list):
 
 test_selection = [selection.roulette]
 test_population = [params.small_population, params.big_population]
-test_elite = [params.small_elite, params.big_elite]
-test_ranked = [params.small_ranked_limit, params.big_ranked_limit]
+test_elite = [params.small_elite]
 test_cross_prob = [params.small_cross_prob, params.big_cross_prob]
-test_dists = [params.norm_dist, params.local_dist, params.global_dist]
+test_dists = [params.norm_dist, params.local_dist]
 test_tournaments = [
     (battle.all_v_all, battle.random_pokemon),
     (battle.all_v_all, battle.first_pokemon),
@@ -49,34 +48,42 @@ test_tournaments = [
 test_cross = [crossover.mixed, crossover.half_split]
 test_succ = [replacement.elite, replacement.generative]
 
+total_tests = len(test_selection) * \
+              len(test_population) * \
+              len(test_elite) * \
+              len(test_cross_prob) * \
+              len(test_dists) * \
+              len(test_tournaments) * \
+              len(test_cross) * \
+              len(test_succ)
+
+i = 1
 for sel in test_selection:
     for pop in test_population:
         for el in test_elite:
-            for rank in test_ranked:
-                for c_prob in test_cross_prob:
-                    for dist in test_dists:
-                        for t in test_tournaments:
-                            for cross in test_cross:
-                                for succ in test_succ:
-                                    logger.info(f'starting test '
-                                                f'selection: {sel.__name__}, '
-                                                f'population: {pop}, '
-                                                f'elite: {el}, '
-                                                f'rank: {rank}, '
-                                                f'crossover prob: {c_prob}, '
-                                                f'mutation prob: {dist}, '
-                                                f'tournament: {(t[0].__name__, t[1].__name__)}, '
-                                                f'crossover: {cross.__name__}, '
-                                                f'succession: {succ.__name__}')
-                                    params.selection = sel
-                                    params.population_size = pop
-                                    params.elite_limit = el
-                                    params.ranked_limit = rank
-                                    params.crossover_probability = c_prob
-                                    params.mutation_dist = dist
-                                    params.tournament_type = t[0]
-                                    params.pokemon_selection = t[1]
-                                    params.crossover = cross
-                                    params.succession = succ
-                                    winner_population = evolve()
-                                    logger.info(f'{winner_population[0]}, pokemons: {winner_population[0].pokemons}')
+            for c_prob in test_cross_prob:
+                for dist in test_dists:
+                    for t in test_tournaments:
+                        for cross in test_cross:
+                            for succ in test_succ:
+                                logger.info(f'starting test no. {i} of {total_tests} '
+                                            f'selection: {sel.__name__}, '
+                                            f'population: {pop}, '
+                                            f'elite: {el}, '
+                                            f'crossover prob: {c_prob}, '
+                                            f'mutation prob: {dist}, '
+                                            f'tournament: {(t[0].__name__, t[1].__name__)}, '
+                                            f'crossover: {cross.__name__}, '
+                                            f'succession: {succ.__name__}')
+                                params.selection = sel
+                                params.population_size = pop
+                                params.elite_limit = el
+                                params.crossover_probability = c_prob
+                                params.mutation_dist = dist
+                                params.tournament_type = t[0]
+                                params.pokemon_selection = t[1]
+                                params.crossover = cross
+                                params.succession = succ
+                                winner_population = evolve()
+                                logger.info(f'{winner_population[0]}, pokemons: {winner_population[0].pokemons}')
+                                i += 1
